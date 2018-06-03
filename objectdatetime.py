108cb9a0a458bfb2a943950e72f13aefa87fe5c9
Alexandra Kulikova <antares.alexx@gmail.com>
2018-06-03 14:41:05 +0300

    добавила ввод даты пользователем и цикл

--- objectdatetime.py
+++ objectdatetime.py
@@ -1,5 +1,21 @@
 from datetime import datetime
 
-user_answer = '01/01/17 12:10:03.234567'
-user_date_time = datetime.strptime(user_answer, '%d/%m/%y %H:%M:%S.%f')
-print(user_date_time)
+try:
+	user_answer = input('Введи нужную дату ')
+	user_date_time = datetime.strptime(user_answer, '%d %m %Y')
+		#user_answer = '01/01/17 12:10:03.234567'
+		#user_date_time = datetime.strptime(user_answer, '%d/%m/%y %H:%M:%S.%f')
+	print(user_date_time)
+except ValueError:
+	print('Как-то непонятно написал.')
+	while True:
+		try:
+			user_answer = input('Введи, пожалуйста, дату в формате "хх хх хххх" ')
+			user_date_time = datetime.strptime(user_answer, '%d %m %Y')
+			print('Вот теперь понятно=)')
+			print(user_date_time)
+			break
+		except ValueError:
+			print('Совсем запутался.')
+		
+
